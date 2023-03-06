0554ac25b6c0fc6a675f94255afaf4c82a08fc77
Péterffy Zoltán <peterffy.zoltan@students.jedlik.eu>
2023-03-06 09:39:39 +0100

    Util Class #1

--- sakk/Utils.cs
+++ sakk/Utils.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace sakk
 {
-    public class Utils
+    public static class Utils
     {
+        static List<Point> PointsOr(List<Point> points1, List<Point> points2) {
+            
+            List<Point> result = new List<Point>();
+            foreach (Point p in points1)
+            {
+                result.Add(p);
+            }
+            foreach (Point  p in points2)
+            {
+                if (!result.Contains(p)) {
+                    result.Add(p);
+                }
+            }
+            return result;
+        }
+        static List<Point> PointsAnd(List<Point> points1, List<Point> points2)
+        {
 
+            List<Point> result = new List<Point>();
+           
+            foreach (Point p in points1)
+            {
+                if (points2.Contains(p))
+                {
+                    result.Add(p);
+                }
+            }
+            return result;
+        }
     }
 }
